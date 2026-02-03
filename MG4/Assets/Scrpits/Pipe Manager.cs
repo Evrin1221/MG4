fb93cdb11955135d2e5a0ec5299b3565f09dee06
Hajin1221 <130419285+Hajin1221@users.noreply.github.com>
2026-02-02 16:27:15 -0800

    pipe spawning system works

--- MG4/Assets/Scrpits/Pipe Manager.cs
+++ MG4/Assets/Scrpits/Pipe Manager.cs
@@ -4,16 +4,32 @@ using UnityEngine;
 
 public class PipeManager : MonoBehaviour
 {
-    [SerializeField] private GameObject[] _pipeVariations; 
+    [SerializeField] private GameObject[] _pipeVariations;
+    [SerializeField] private float _spawnInterval;
+
+    private GameObject _pipe;
+    private float _timer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _pipe = _pipeVariations[SelectPipe()];
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _timer += Time.deltaTime;
+        if (_timer >= _spawnInterval)
+        {
+            Instantiate(_pipe,transform.position,Quaternion.identity);
+            _pipe = _pipeVariations[SelectPipe()];
+            _timer = 0;
+        }
+    }
+
+    private int SelectPipe()
+    {
+        int _pipeIndex = Random.Range(0, _pipeVariations.Length);
+        return _pipeIndex;
     }
 }
