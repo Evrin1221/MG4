be9f661159844eced62d8af9f9632277bf934ca1
Hajin1221 <130419285+Hajin1221@users.noreply.github.com>
2026-02-02 17:25:08 -0800

    locator in progress

--- MG4/Assets/Scrpits/Locator.cs
+++ MG4/Assets/Scrpits/Locator.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class Locator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+ public static Locator Instance {  get; private set; }
+    public Player _player { get; private set; }
+
+
+    private void Awake()
     {
-        
+        if (Instance!= null && Instance != this)
+        {
+            Destroy(this);
+            return;
+
+        }
+        Instance = this;
+        GameObject player = GameObject.FindWithTag("Player");
     }
 
     // Update is called once per frame
